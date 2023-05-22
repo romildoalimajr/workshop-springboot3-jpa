ff60497c4aa25ff8462c3cf27689e752c17d8adf
Romldo A. Lima Jr <odlimor.sevla@gmail.com>
2023-05-22 19:33:16 -0300

    Service layer, component registration

--- course/src/main/java/net/kalangos/course/repositories/UserRepository.java
+++ course/src/main/java/net/kalangos/course/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package net.kalangos.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import net.kalangos.course.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
---
+++ course/src/main/java/net/kalangos/course/services/UserService.java
@@ -0,0 +1,27 @@
+package net.kalangos.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.kalangos.course.entities.User;
+import net.kalangos.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
