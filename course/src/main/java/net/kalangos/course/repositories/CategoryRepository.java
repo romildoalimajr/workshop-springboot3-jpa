01996a589beef20d5dda8ad67c569b42e6b0c0e9
Romldo A. Lima Jr <odlimor.sevla@gmail.com>
2023-05-23 19:39:13 -0300

    Category

---
+++ course/src/main/java/net/kalangos/course/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package net.kalangos.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.kalangos.course.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
