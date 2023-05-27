4b23be38207c77eb760514b6c46d05e01f507e9f
Romldo A. Lima Jr <odlimor.sevla@gmail.com>
2023-05-27 00:23:36 -0300

    User Delete

--- course/src/main/java/net/kalangos/course/resource/UserResource.java
+++ course/src/main/java/net/kalangos/course/resource/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,7 +27,7 @@ public class UserResource {
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
 		
-		List<User> list = service.findAll();
+		List<User> list = service.findAll(
 		
 		return ResponseEntity.ok().body(list);
 	}
@@ -44,6 +45,12 @@ public class UserResource {
 				.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
