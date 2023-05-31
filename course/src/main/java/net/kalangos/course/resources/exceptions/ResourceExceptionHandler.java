491e9ef4867806d9bb5e7e7930913114ad166cb6
Romldo A. Lima Jr <odlimor.sevla@gmail.com>
2023-05-31 17:47:35 -0300

    Exception handling - delete

--- course/src/main/java/net/kalangos/course/resources/exceptions/ResourceExceptionHandler.java
+++ course/src/main/java/net/kalangos/course/resources/exceptions/ResourceExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import jakarta.servlet.http.HttpServletRequest;
+import net.kalangos.course.services.exceptions.DatabaseException;
 import net.kalangos.course.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -17,6 +18,15 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotound(ResourceNotFoundException e, HttpServletRequest request){
 		String error = "Resource not found!";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Database error!";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
